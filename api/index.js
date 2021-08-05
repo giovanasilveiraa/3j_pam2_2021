845f48cdf926150003527ff073f7cf97857a1293
giovanasilveiraa <giovana.silveira01@etec.sp.gov.br>
2021-08-04 22:52:27 -0300

    Inicio da criação do servidor da api

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const servidor = express()
+
+servidor.get(
+    '/', (req, res, next) =>
+ {
+    return res.send(
+        {
+            "mensagem": "Bem-vindo(a) ao servidor"
+        }
+    )
+ }
+
+)
+
+servidor.listen(3000, () => {
+    console.log('Servidor funcionando!!!')
+})
